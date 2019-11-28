bccdb5808beffcd7bab7309938b12e9abc24e041
Cindy Kei <myk245@nyu.edu>
2019-11-28 03:05:18 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,6 +1,6 @@
 def map_to_negativize(source_array)
   new_array = []
-  new_values = source_array.each do {x * -1}
+  new_values = source_array.each do {|x| * -1}
   end
   new_array << new_values
   return new_array
