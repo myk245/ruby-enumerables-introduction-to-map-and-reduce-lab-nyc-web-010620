087334573953f67614523f0dc0342a05f9c09418
Cindy Kei <myk245@nyu.edu>
2019-11-28 04:28:30 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -45,6 +45,7 @@ def reduce_to_any_true(source_array)
   i = 0
   while i < source_array.length do
     return true if source_array[i] == true
+    i += 1 
   end
-  return false 
+  return false
 end
