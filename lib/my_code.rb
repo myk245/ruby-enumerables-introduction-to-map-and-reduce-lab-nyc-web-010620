904495c54a6e0c28cc1e6c16ada4b4c7a280d887
Cindy Kei <myk245@nyu.edu>
2019-11-28 03:54:46 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -28,6 +28,7 @@ def map_to_square(source_array)
   return new_array
 end
 
-def reduce_to_total(source_array, starting_point = 0)
+def reduce_to_total(source_array, starting_point)
+  starting_point = 0
   source_array.sum(starting_point)
 end
